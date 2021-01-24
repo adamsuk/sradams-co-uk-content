87ef9bdbe63849227b0a6d265632db994eb50703
Scott <sra405@gmail.com>
2021-01-24 08:26:48 +0000

    homepage image

--- pages/about.js
+++ pages/about.js
@@ -4,9 +4,22 @@ import Link from 'next/link'
 import Layout from '../components/Layout'
 
 const About = () => <Layout title="About Me">
-      <p>
-        TBC
-      </p>
-      </Layout>;
+  <div>
+  <p>
+    <br></br>
+    Hello World.
+    <br></br>
+    <br></br>
+    So I'm new to JavaScript, Next.js and most things front-end.<br></br>
+    This is a little personal project of mine to get up to speed.
+    <br></br>
+    <br></br>
+    By all means take a look at how it's coming on and <a href="https://github.com/adamsuk/sradams-co-uk-content">visit on Github!</a>
+    <br></br>
+    <br></br>
+    See you soon!
+  </p>
+  </div>
+  </Layout>;
 
 export default About
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,17 @@
 import { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
+import Image from 'next/image'
+
 import Layout from '../components/Layout'
 
 
 const Index = () => <Layout>
-  <div>
-  <p>
-    <br></br>
-    Hello World.
-    <br></br>
-    <br></br>
-    So I'm new to JavaScript, Next.js and most things front-end.<br></br>
-    This is a little personal project of mine to get up to speed.
-    <br></br>
-    <br></br>
-    By all means take a look at how it's coming on and <a href="https://github.com/adamsuk/sradams-co-uk-content">visit on Github!</a>
-    <br></br>
-    <br></br>
-    See you soon!
-  </p>
-  </div>
+    <Image
+        src="../public/me.png"
+        alt="Picture of ME!"
+        width={500}
+        height={500}
+    />
   </Layout>;
 
 export default Index
