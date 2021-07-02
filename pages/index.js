30eea5158294a438c46c8956158b36179b383703
Scott <sra405@hotmail.co.uk>
2021-07-03 00:29:21 +0100

    feat(updated home page): some sweet css

--- pages/index.js
+++ pages/index.js
@@ -5,27 +5,27 @@ import style from "../components/index.module.scss";
 
 
 const Index = () => <Layout>
-    <Image
-        src="/me.jpg"
+  <div className={style.item}>
+    <a>
+    <img
+        src="https://github.com/adamsuk.png"
         alt="Picture of ME!"
-        width={500}
-        height={500}
     />
-  <div className={style.item}> 
     <p>
-      <br></br>
-        Hello World!
-      <br></br>
-      <br></br>
-        So I'm new to JavaScript, Next.js and most things front-end.<br></br>
-        This is a little personal project of mine to get up to speed.
+      Hello World!
       <br></br>
       <br></br>
-        By all means take a look at how it's coming on and <a href="https://github.com/adamsuk/sradams-co-uk-content">visit on Github!</a>
+      So I'm new to JavaScript, Next.js and most things front-end.
+      <br></br>
+      This is a little personal project of mine to get up to speed.
       <br></br>
       <br></br>
-        See you soon!
+      Feel free to snoop around and by all means keep coming back, I'm learning daily!
+      <br></br>
+      <br></br>
+      See you soon
     </p>
+    </a>
   </div>
   </Layout>;
 
