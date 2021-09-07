4a9d275978c691990bc126f7e221438354919e18
Scott Adams <scott_adams080291@hotmail.com>
2021-09-07 19:40:29 +0100

    Initial Login Portal PoC (#20)
    * feat(initial firebase auth): trialling firebase sdk based auth for login portal
    
    * fix(adding postcss dependencies): explicit dependencies needed
    
    * fix(firebase version): update to last v7 firebase release
    
    Co-authored-by: Scott <sra405@hotmail.co.uk>

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
 
 module.exports = {
-    webpack: function(config, { isServer }) {
-      // Fixes npm packages that depend on `fs` module
-      if (!isServer) {
-        config.node = {
-          fs: 'empty'
-        }
-      };
-      config.module.rules.push({
-        test: /\.md$/,
-        use: 'raw-loader',
-      })
-      return config
-    }
+  webpack: function(config, { isServer }) {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.node = {
+        fs: 'empty'
+      }
+    };
+    config.module.rules.push({
+      test: /\.md$/,
+      use: 'raw-loader',
+    })
+    return config
   }
+}
