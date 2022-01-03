0007a55b5ac331372a9d587ecd06186c931e7124
Scott Adams <scott_adams080291@hotmail.com>
2022-01-03 23:03:13 +0000

    Create 404.js
    Coding blind, let's give this a whirl

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+export default function Custom404() {
+  return (
+    <div className="flex justify-center items-center h-screen max-w-7xl w-full p-7">
+      <div className="m-auto text-center content-center md:max-w-[50%]">
+        <h1>Uh oh - Page Not Found</h1>
+        <a>You've either clicked a TBC link or chanced it with a path that doesn't exist. Either way no page here.</a>
+      </div>
+    </div>
+  )
+}
