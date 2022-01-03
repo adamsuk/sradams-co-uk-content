e1ec5471a96018445408dae6df41db4365dafcd1
Scott Adams <scott_adams080291@hotmail.com>
2022-01-03 23:29:58 +0000

    Update 404.js
    Give using markdown to render emojis a whirl!

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,16 @@
+import Markdown from 'react-markdown'
+
 export default function Custom404() {
   return (
     <div className="flex justify-center items-center h-screen max-w-7xl w-full p-7">
+      <Markdown 
+        rehypePlugins={[rehypeRaw, rehypeSanitize]}
+        parserOptions={{ commonmark: true }}
+        className="prose dark:prose-invert whitespace-no-wrap"
+      >
+        :sob:
+      </Markdown>
+      <br></br>
       <div className="m-auto text-center content-center md:max-w-[50%]">
         <h1>Uh oh - Page Not Found</h1>
         <a>You've either clicked a TBC link or chanced it with a path that doesn't exist. Either way no page here.</a>
