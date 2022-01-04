9e702e61a1935ea93df548cf89c55f27220f1568
Scott Adams <scott_adams080291@hotmail.com>
2022-01-04 00:04:06 +0000

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -4,7 +4,7 @@ export default function Custom404() {
   return (
     <div className="flex flex-wrap flex-col justify-center items-center h-screen max-w-7xl w-full p-7">
       <div className="flex-1 flex-col m-auto text-center content-center md:max-w-[50%]">
-        <a className="text-[25%]">😭</a>
+        <a className="text-[10rem]">😭</a>
         <br></br>
         <h1>Uh oh - Page Not Found</h1>
         <a>You've either clicked a TBC link or chanced it with a path that doesn't exist. Either way no page here.</a>
