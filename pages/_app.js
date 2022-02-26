a59e6a23a7a11f9292ec44c6353b302ca5f2a53a
Scott <sra405@hotmail.co.uk>
2022-02-26 19:52:09 +0000

    fix(bottom-nav): getting scales and sticky better across the board

--- pages/_app.js
+++ pages/_app.js
@@ -12,14 +12,14 @@ class MyApp extends App {
     const { Component, pageProps } = this.props;
     return (
       <ThemeProvider attribute='class' className='absolute inset-0'>
-        <div  key="generic" className='flex flex-col'>
+        <div  key="generic" className='flex flex-col h-screen'>
           <Head>
             <title>Scott Adams</title>
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <meta charSet="utf-8" />
           </Head>
           <Header />
-          <Component {...pageProps} />
+          <Component {...pageProps} className="mb-auto" />
           <NavBar className="sticky bottom-0" />
         </div>
       </ThemeProvider>
