610b510dd75c81eed73e38b3c24694f5376bb498
Scott Adams <sra405@hotmail.co.uk>
2026-04-01 19:13:07 +0100

    remove test coverage hard limit

--- jest.config.js
+++ jest.config.js
@@ -1,38 +1,38 @@
-const nextJest = require('next/jest');
+const nextJest = require("next/jest");
 
-const createJestConfig = nextJest({ dir: './' });
+const createJestConfig = nextJest({ dir: "./" });
 
 /** @type {import('jest').Config} */
 const customJestConfig = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  testEnvironment: "jest-environment-jsdom",
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    "^@/(.*)$": "<rootDir>/$1",
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
     // Map ESM-only packages to CJS mocks so Jest can load them
-    '^rehype-raw$': '<rootDir>/__mocks__/pluginMock.js',
-    '^rehype-sanitize$': '<rootDir>/__mocks__/pluginMock.js',
-    '^remark-gfm$': '<rootDir>/__mocks__/pluginMock.js',
+    "^rehype-raw$": "<rootDir>/__mocks__/pluginMock.js",
+    "^rehype-sanitize$": "<rootDir>/__mocks__/pluginMock.js",
+    "^remark-gfm$": "<rootDir>/__mocks__/pluginMock.js",
   },
   collectCoverageFrom: [
-    'pages/**/*.{ts,tsx}',
-    'components/**/*.{ts,tsx}',
-    'default-env.ts',
-    '!pages/_app.tsx',
-    '!pages/_document.tsx',
+    "pages/**/*.{ts,tsx}",
+    "components/**/*.{ts,tsx}",
+    "default-env.ts",
+    "!pages/_app.tsx",
+    "!pages/_document.tsx",
     // MediaPlayer is commented out and not integrated into the app
-    '!components/sandbox/MediaPlayer.tsx',
-    '!**/*.d.ts',
+    "!components/sandbox/MediaPlayer.tsx",
+    "!**/*.d.ts",
   ],
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
-    },
-  },
-  coverageReporters: ['text', 'json-summary'],
+  // coverageThreshold: {
+  //   global: {
+  //     branches: 80,
+  //     functions: 80,
+  //     lines: 80,
+  //     statements: 80,
+  //   },
+  // },
+  coverageReporters: ["text", "json-summary"],
 };
 
 module.exports = createJestConfig(customJestConfig);
